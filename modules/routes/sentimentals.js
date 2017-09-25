f3258b94d666e9d135735f5d366102d7bc8bfc55
devjanaprime <dev@primeacademy.io>
2017-09-25 13:28:01 -0500

    step06

--- modules/routes/sentimentals.js
+++ modules/routes/sentimentals.js
@@ -4,11 +4,11 @@ var router = express.Router();
 var sentimentals = [];
 
 // starting objects
-sentimentals.push( { image: 'http://localhost:7654/pics/moments_hirsch.png', synopsis: 'test1', hoorays: 0 } );
-sentimentals.push( { image: 'http://localhost:7654/pics/moments_anjie.png', synopsis: 'test3', hoorays: 0 } );
-sentimentals.push( { image: 'http://localhost:7654/pics/moments_aaron.png', synopsis: 'test3', hoorays: 0 } );
-sentimentals.push( { image: 'http://localhost:7654/pics/moments_canopus.png', synopsis: 'test3', hoorays: 0 } );
-sentimentals.push( { image: 'http://localhost:7654/pics/moments_krystal.png', synopsis: 'test3', hoorays: 0 } );
+sentimentals.push( { image: 'http://10.100.100.104:7654/pics/moments_hirsch.png', synopsis: 'test1', hoorays: 0 } );
+sentimentals.push( { image: 'http://10.100.100.104:7654/pics/moments_anjie.png', synopsis: 'test3', hoorays: 0 } );
+sentimentals.push( { image: 'http://10.100.100.104:7654/pics/moments_aaron.png', synopsis: 'test3', hoorays: 0 } );
+sentimentals.push( { image: 'http://10.100.100.104:7654/pics/moments_canopus.png', synopsis: 'test3', hoorays: 0 } );
+sentimentals.push( { image: 'http://10.100.100.104:7654/pics/moments_krystal.png', synopsis: 'test3', hoorays: 0 } );
 
 router.get( '/', function( req, res ){
     console.log( '/sentimentals route get hit' );
